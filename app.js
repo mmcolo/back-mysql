5ecfc1b33c64fed5f6384aad24b57df29e082ab4
mmcolo <mcolo.miftahou@gmail.com>
2020-10-19 17:13:43 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql'); 
+
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'turbulences'
+});
+
+const app = express();
+
+app.use((req, res, next)=>{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Header', 'Origin, X-requested-With, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow','GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+con. connect((err)=>{
+    if(err) throw err;
+    console.log("Connected!");
+});
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.get('/api/users', (req, res)=>{
+    let sql="SELECT * FROM  utilisateur"
+    con.query(sql, (err, result)=>{
+        if(err){
+            res.status(400).json({err});
+        }
+        res.status(200).json({result})
+        console.log(result);
+    })
+});
+
+module.exports = app;
